6c379886944cf86e2adc27da29ccf33866d1eb84
Bertie <berniesnell@gmail.com>
2020-09-07 09:58:33 +0100

    refactor(utils): use format to create and join html elements

--- src/server/handlers/utils.rs
+++ src/server/handlers/utils.rs
@@ -6,6 +6,8 @@ use super::super::super::drawing_app::{application, canvas, commands};
 use super::errors;
 use super::request;
 
+/// Attempts to apply draw operation to the canvas
+/// Returns a Result type
 pub fn apply_draw_operation(
     command: Vec<commands::DrawCommand>,
     app: Arc<RwLock<application::DrawingApplication>>,
@@ -25,41 +27,21 @@ pub fn apply_draw_operation(
     }
 }
 
-/// Check that the body is JSON and marshalls into correct format (and reject huge payloads)
+/// Check that the body is JSON and marshalls into correct draw rectangle request format
+/// Rejects big payloads
 pub fn parse_draw_rectangle_request() -> impl Filter<Extract = (request::DrawRectangleOperation,), Error = warp::Rejection> + Clone {
     warp::body::content_length_limit(1024 * 16).and(warp::body::json())
 }
 
-/// Check that the body is JSON and marshalls into correct format (and reject huge payloads)
+/// Check that the body is JSON and marshalls into correct flood fill request format
+/// Rejects big payloads
 pub fn parse_flood_fill_request() -> impl Filter<Extract = (request::FloodFillOperation,), Error = warp::Rejection> + Clone {
     warp::body::content_length_limit(1024 * 16).and(warp::body::json())
 }
 
-pub fn construct_html_with_canvas(canvas: &canvas::Canvas) -> String {
-    let mut start_tags: String = String::from("
-        <html>
-            <head>
-                <title>HTML with warp!</title>
-            </head>
-        <body>"
-    );
-    let canvas_pixels: String = convert_canvas_to_html(canvas);
-    let end_tags = String::from("
-        <script type=\"text/javascript\">var uri = 'ws://' + location.host + '/canvas';
-            var ws = new WebSocket(uri);
-            console.log(ws);
-            ws.onmessage = function(msg) {
-                console.log(msg.data);
-            };
-        </script>
-    </body></html>");
-    start_tags.push_str(&canvas_pixels);
-    start_tags.push_str(&end_tags);
-    start_tags
-}
-
+/// Constructs a valid HTML element out of the canvas
 pub fn convert_canvas_to_html(canvas: &canvas::Canvas) -> String { 
-    canvas.pixels
+    let canvas_element = canvas.pixels
         .iter()
         .map(|row| {
             let mut html_string = String::from("<div>");
@@ -70,5 +52,23 @@ pub fn convert_canvas_to_html(canvas: &canvas::Canvas) -> String {
             html_string.push_str(&String::from("</div>"));
             html_string
         })
-        .fold(String::new(), |a, b| a + &b + "\n")
+        .fold(String::new(), |a, b| a + &b + "\n");
+
+    format!("{}{}{}","<div class=\"canvas\"", canvas_element, "div")
+}
+
+/// Constructs a basic HTML document containing the canvas
+/// Contains basic web socket initialisation scripts 
+pub fn construct_html_with_canvas(canvas: &canvas::Canvas) -> String {
+    let start_tags: String = String::from("
+        <html>
+            <head>
+                <title>HTML with warp!</title>
+            </head>
+        <body>"
+    );
+    let canvas_element: String = convert_canvas_to_html(canvas);
+    let end_tags = String::from("</body></html>");
+
+    format!("{}{}{}", start_tags, canvas_element, end_tags)
 }
